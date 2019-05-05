1fd309f80e06b7852a8482860e5cc5445309a969
sheng <1>
2019-05-05 20:40:17 +0800

    头部组件完成“

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Button } from 'antd-mobile';
+import Header from './common/header'
 
 class Base extends React.PureComponent {
   render () {
     return (
       <div>
-        <Button type="primary">点击</Button>
+        <Header></Header>
       </div>
     )
   }
---
+++ src/common/header/style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.header`
+  height: 50px;
+  color: #fff;
+  background: #e54847;
+  text-align: center;
+  line-height: 50px;
+`
+
+export const HeaderH1 = styled.h1`
+  font-size: 18px;
+`
